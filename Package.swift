4b916cfc712b551545cce52c828ef7007c3a7ba3
Samet Koyuncu <sametkoyuncu@live.com>
2024-07-13 12:59:35 +0300

    Add package swift file

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "RouteTown",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "RouteTown",
+            targets: ["RouteTown"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "RouteTown",
+            url: "https://github.com/sametkoyuncu/RouteTown/releases/download/v0.1.0/RouteTown.xcframework.zip",
+            checksum: "381aa46eff3f3dba05784459dd1dda03536ec466227767dfd58bb71c6f952351"
+        ),
+    ]
+)
